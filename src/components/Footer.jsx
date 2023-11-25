ea83929d37143a960497040584e9b544d793795a
blbecheli <blbecheli@gmail.com>
2023-11-24 16:23:27 -0800

    fixed imgs, text and css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
           </p>
           <p>Email:
             <span>
-              <a href="mailto:omblanco80@gmail.com">omar@omar.com</a>
+              <a href="mailto:omblanco.contracting@gmail.com">omblanco.contracting@gmail.com</a>
             </span>
           </p>
           <img src="./img/logo3.png" alt="logo" className={style.footer__logo} />
@@ -24,13 +24,13 @@ const Footer = () => {
         <div className={style.footer__social}>
           <p>Social</p>
           <div>            
-            <a href="https://www.instagram.com/blancoinsulation/" target="_blank" rel="noreferrer"><img src="./img/instagram.svg" alt="instagram" /></a>
+            <a href={screen.width >= 1024 ? "https://web.whatsapp.com/send?phone=12369995790" : "https://api.whatsapp.com/send?phone=12369995790"} target="_blank" rel="noreferrer"><img src="./img/instagram.svg" alt="instagram" /></a>
             <a href="https://web.whatsapp.com/send?phone=+12369995790" target="_blank" rel="noreferrer"><img src="./img/whatsapp.svg" alt="whatsapp" /></a>
           </div>
         </div>
       </div>
       <div className={style.company}>
-        <p> Blanco Insulation © 2023. All Rights Reserved </p>
+        <p> Blanco Insulation © 2023. All Rights Reserved - Developed by BLB </p>
       </div>
     </footer>
   )
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,8 +3,22 @@ import styles from './Contact.module.css'
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1>Contact Us</h1>
+      <h1>It will be a pleasure to assist you</h1>
+      <h4>Contact us via WhatsApp, Instagram, or email</h4>
+      <div className={styles.contact}>
+
+        <a href={screen.width >= 1024 ? "https://web.whatsapp.com/send?phone=12369995790" : "https://api.whatsapp.com/send?phone=12369995790"} target="_blank" rel="noreferrer">
+          <img src="./img/whatsapp.svg" alt="whatsapp" />
+        </a>
+        <a href="https://www.instagram.com/blancoinsulation" target="_blank" rel="noreferrer">
+          <img src="./img/instagram.svg" alt="instagram" />
+        </a>
+        <a href="mailto:omblanco.contracting@gmail.com">
+          <img src="/img/email.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
+
 export default Contact
